ca779d297bd15bf6df6931cb9feb256972f046af
lynn pepin <lynnpepin@tutanota.com>
2024-12-02 19:08:45 -0500

    feat: render a circle

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,14 @@
 #[cfg(feature = "custom_cursor")]
 use bevy::winit::cursor::CustomCursor;
 use bevy::{
-  input::gamepad::{
+  core::FrameCount, diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}, input::{gamepad::{
     GamepadAxisChangedEvent, GamepadButtonChangedEvent, GamepadButtonStateChangedEvent,
     GamepadConnectionEvent, GamepadEvent,
-  },
-  input::keyboard::KeyboardInput,
-  core::FrameCount,
-  diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
-  window::{CursorGrabMode, PresentMode, SystemCursorIcon, WindowLevel, WindowTheme},
-  winit::cursor::CursorIcon,
-  prelude::*,
+  }, keyboard::KeyboardInput}, prelude::*, window::{CompositeAlphaMode, CursorGrabMode, PresentMode, SystemCursorIcon, WindowLevel, WindowTheme}, winit::cursor::CursorIcon
 };
 
-mod grist;
-use grist::*;
+mod print_input;
+use print_input::*;
 mod window_utils;
 use window_utils::*;
 
@@ -39,6 +33,16 @@ fn main() {
               minimize: true,
               close: true
             },
+            // see: https://github.com/bevyengine/bevy/blob/main/examples/window/transparent_window.rs
+            // Setting `transparent` allows the `ClearColor`'s alpha value to take effect
+            transparent: true,
+            // Disabling window decorations to make it feel more like a widget than a window
+            decorations: true,
+            #[cfg(target_os = "macos")]
+            composite_alpha_mode: CompositeAlphaMode::PostMultiplied,
+            #[cfg(target_os = "linux")]
+            composite_alpha_mode: CompositeAlphaMode::PreMultiplied,
+            // ClearColor must have 0 alpha, otherwise some color will bleed through
             visible: false,
             ..default()
           }),
@@ -46,6 +50,7 @@ fn main() {
         }),
       )
     )
+    .insert_resource(ClearColor(Color::NONE)) // for transparent window
     .add_systems(
       Update,
       (
@@ -67,14 +72,37 @@ fn main() {
         make_visible,
       )
     )
+    .add_systems(Startup, setup)
     .run();
+}
+
+
+// https://bevyengine.org/examples/2d-rendering/2d-shapes/
+fn setup(
+  mut commands: Commands,
+  mut meshes: ResMut<Assets<Mesh>>,
+  mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+  commands.spawn(Camera2d);
+
+  commands.spawn(
+    (
+      Mesh2d(
+        meshes.add(Circle::new(4.))
+      ),
+      MeshMaterial2d(
+        materials.add(
+          Color::hsv(0.0, 1.0, 1.0)
+        )
+      ),
+      Transform::from_xyz(8.0, 8.0, 0.0)
+    )
+  );
+
 }
 
 /*
 TODO:
-- Window stuff:
-    - https://github.com/bevyengine/bevy/blob/main/examples/window/window_settings.rs
-    - Move all of the window commands like "make_visible", etc. into a "window_utils.rs" mod
 - Genuary "particles"
 - WASM export, selfhost
 */
